dd0c0f8795deb2b48021eb7e1a1b8625a2d0f48b
flavorfan <fanck1979@gmail.com>
2020-04-09 15:26:10 +0800

    update the learning read

---
+++ tf_dataset/tfrecord_tiny_imagenet.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2020/4/8 15:52
+# @Author  : Flavorfan
+# @File    : tfrecord_tiny_imagenet.py
+
+import tensorflow as tf
+import tensorflow_datasets as tfdt
+
+
+if __name__ == '__main__':
+    print(tf.__version__)
+    print(tfdt.__version__)
